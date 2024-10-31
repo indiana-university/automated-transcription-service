eebd46ca5797b02b814b76b838fa2423f5f84998
Alan Walsh <alwalsh@iu.edu>
2024-10-31 15:03:32 -0400

    Name prefix and some setup for step func (#23)

--- aws/terraform/sqs.tf
+++ aws/terraform/sqs.tf
@@ -1,5 +1,5 @@
 resource "aws_sqs_queue" "transcribe_to_docx" {
-  name                              = "${var.lambda_docx}-queue"
+  name                              = "${var.prefix}-${var.lambda_docx}-queue"
   receive_wait_time_seconds         = 20
   content_based_deduplication       = false
   delay_seconds                     = 0
@@ -44,7 +44,7 @@ resource "aws_sqs_queue_policy" "transcribe_to_docx" {
 }
 
 resource "aws_sqs_queue" "audio_to_transcribe" {
-  name                              = "${var.lambda_ts}-queue"
+  name                              = "${var.prefix}-${var.lambda_ts}-queue"
   content_based_deduplication       = false
   delay_seconds                     = 0
   fifo_queue                        = false
@@ -94,7 +94,7 @@ resource "aws_sqs_queue_policy" "audio_to_transcribe" {
 }
 
 resource "aws_sqs_queue" "transcribe_to_docx_dlq" {
-  name                              = "${var.lambda_docx}-dlq"
+  name                              = "${var.prefix}-${var.lambda_docx}-dlq"
   content_based_deduplication       = false
   delay_seconds                     = 0
   fifo_queue                        = false
@@ -130,7 +130,7 @@ resource "aws_sqs_queue_policy" "transcribe_to_docx_dlq" {
 }
 
 resource "aws_sqs_queue" "audio_to_transcribe_dlq" {
-  name                              = "${var.lambda_ts}-dlq"
+  name                              = "${var.prefix}-${var.lambda_ts}-dlq"
   content_based_deduplication       = false
   delay_seconds                     = 0
   fifo_queue                        = false
