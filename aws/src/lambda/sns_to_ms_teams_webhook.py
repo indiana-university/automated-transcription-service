2ab698af3247f13821f49f7d6603f76deab528eb
Philip Berg <pcberg@iu.edu>
2025-02-10 14:25:25 -0500

    Update sns_to_ms_teams_webhook.py

--- aws/src/lambda/sns_to_ms_teams_webhook.py
+++ aws/src/lambda/sns_to_ms_teams_webhook.py
@@ -3,13 +3,15 @@ from urllib.request import urlopen
 from urllib.request import Request
 
 def lambda_handler(event, context):
+    const webhook = os.environ['WEBHOOK_URL']
+
     if not webhook or webhook == "DISABLED":
         print(f"notify_teams disabled, logging message instead: {title} {content}")
         return 200
 
     msg = {'text': event['Records'][0]['Sns']['Message']}
     headers = {'Content-Type': 'application/json'}
-    response = requests.post(os.environ['WEBHOOK_URL'], headers=headers, data=json.dumps(msg).encode('utf-8'))
+    response = requests.post(webhook, headers=headers, data=json.dumps(msg).encode('utf-8'))
     return {
         'statusCode': 200,
         'body': "Teams message sent!"
