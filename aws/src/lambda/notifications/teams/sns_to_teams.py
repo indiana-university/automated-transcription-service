40ad7decda5034abbf9de424217073d25119db41
pcberg <pcberg@users.noreply.github.com>
2026-04-27 10:22:49 -0400

    feat 20: MS connectors -> Power Automate (#62)
    * feat 20: MS connectors -> Power Automate
    
    * 20: Increase width & success / failure rendering
    
    ---------
    
    Co-authored-by: Alan Walsh <alwalsh@iu.edu>

--- aws/src/lambda/notifications/teams/sns_to_teams.py
+++ aws/src/lambda/notifications/teams/sns_to_teams.py
@@ -39,8 +39,49 @@ def lambda_handler(event, context):
 
     job_name = body.get("job", "Unknown Job")
     s3uri = body.get("s3uri", "No S3 URI provided")
-    teams_message = f"Job Name:<br><pre>{job_name}</pre><br>Transcript available at:<br><pre>{s3uri}</pre>"
-    message = {"summary": title, "sections": [{"activityTitle": title, "activitySubtitle": teams_message}]}
+    title_lower = title.lower()
+    if "fail" in title_lower or "stop" in title_lower or "error" in title_lower:
+        header_style = "attention"
+    else:
+        header_style = "good"
+    message = {
+        "type": "message",
+        "attachments": [
+            {
+                "contentType": "application/vnd.microsoft.card.adaptive",
+                "contentUrl": None,
+                "content": {
+                    "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+                    "type": "AdaptiveCard",
+                    "version": "1.4",
+                    "msteams": {"width": "Full"},
+                    "body": [
+                        {
+                            "type": "Container",
+                            "style": header_style,
+                            "bleed": True,
+                            "items": [
+                                {
+                                    "type": "TextBlock",
+                                    "size": "Medium",
+                                    "weight": "Bolder",
+                                    "text": title,
+                                    "wrap": True,
+                                },
+                            ],
+                        },
+                        {
+                            "type": "FactSet",
+                            "facts": [
+                                {"title": "Job Name", "value": job_name},
+                                {"title": "Transcript S3 URI", "value": s3uri},
+                            ],
+                        },
+                    ],
+                },
+            }
+        ],
+    }
     request_data = json.dumps(message).encode("utf-8")
     req = Request(url=webhook, headers={"Content-Type": "application/json"}, data=request_data, method='POST')
 
