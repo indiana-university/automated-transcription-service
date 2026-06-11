c542a2c8bbb5c3c5000aca50acb2ea96390dc02f
Alan Walsh <alwalsh@iu.edu>
2026-06-11 14:08:19 -0400

    Clarify supported Python versions

--- aws/terraform/variables.tf
+++ aws/terraform/variables.tf
@@ -83,7 +83,7 @@ variable "slack_notification" {
 }
 
 variable "python_version" {
-  description = "Python version to use for the Lambda functions. Supported versions: 3.12 or later"
+  description = "Python version to use for the Lambda functions. Supported versions: 3.13 or later"
   type        = string
   default     = "3.13"
 }
